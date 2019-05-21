4f7fbf0178850a18a1b590d68a0c9ecaa664ca2a
Gasper <duanpengyuan@qq.com>
2019-05-21 14:29:32 +0800

    增加影像注记

--- server/public/js/home.js
+++ server/public/js/home.js
@@ -1,6 +1,6 @@
 
 var map_opts = {
-    center: L.latLng(30.65984, 104.10194),
+    center: L.latLng(30.65984, 104.10194),      // 成都
     zoom: 2,
     minZoom: 1,
     maxZoom: 14,
@@ -12,6 +12,7 @@ var map_our = L.map('map_our', map_opts)
 var map_std = L.map('map_std', map_opts)
 
 L.tileLayer('/tile/img/{z}/{x}/{y}').addTo(map_our)
+L.tileLayer('/tile/cia/{z}/{x}/{y}').addTo(map_our)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map_std)
 
 map_our.addLayer(L.gridLayer.debugCoords())
